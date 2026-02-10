1b2dd3ddc196eccf863f3703615d383f322420a4
Denisse Roka <denisserokavalido@gmail.com>
2026-02-10 19:26:37 +0000

    correct readonly tests

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -200,7 +200,7 @@ class TestApi(unittest.TestCase):
         )
         print('End - integration test Delete TODO')
     
-        @pytest.mark.readonly
+    @pytest.mark.readonly
     def test_api_listtodos_readonly(self):
         print('---------------------------------------')
         print('Starting - READONLY integration test List TODO')
